88768f953b3c5dba2298f8878330fdfc0502efe0
striker1101 <goodluckodemilin@gmail.com>
2024-08-22 11:01:47 +0100

    initial template final

---
+++ server/controllers/indexController.js
@@ -0,0 +1,8 @@
+// const auth = require("../mongoDB/users");
+
+exports.index = async (req, res) => {}; // get all | list all
+exports.show = async (req, res) => {}; // show specifix auth
+exports.create = async (req, res) => {}; // create a new auth
+exports.edit = async (req, res) => {}; // edit a auth
+exports.update = async (req, res) => {}; // update a auth
+exports.delete = async (req, res) => {}; // delete a auth
